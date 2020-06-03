795f728f597768497e10d3d24e68b4b290ac04bc
Facundo HM <facundo.hmz@gmail.com>
2020-06-03 00:43:48 -0300

    Implement fix range iteration

--- color_shape_text/color_shape_text.py
+++ color_shape_text/color_shape_text.py
@@ -1,10 +1,13 @@
 from typing import Dict, Tuple, Iterable
-import string
 from itertools import product
+from math import sqrt
+import string
+import random
 
 # 8-bit colors
 COLORS_AMOUNT = 256
-MAX_TEXT_LENGTH = 6
+MAX_TEXT_LENGTH = 30
+MAX_COLORS_PER_LETTER = 2
 
 letters = list(string.ascii_lowercase)
 colors = tuple(range(COLORS_AMOUNT))
@@ -23,14 +26,30 @@ colors_by_letters: Dict[str, Tuple[int, ...]] = {
 def combine_colors(text: str) -> Iterable[Tuple[int, ...]]:
     letter_tuples: List[Tuple[int, ...]] = []
 
-    for letter in text:
-        letter_tuples.append(colors_by_letters.get(letter, (0,)))
+    # instead of add all the colors, add cartain amount of colors per letter randomly
+    # no matter how big or small is the text, always display the same amoutn of colors
+    # iterate over a fix range
+    # use letters to get colors in each iteration
+    for _ in range(3):
+        text_colors = []
+
+        for letter in text:
+            color_choice = random.choice(
+                colors_by_letters.get(letter, (0,))
+            )
+            text_colors.append(color_choice)
+
+        letter_tuples.append(text_colors)
+
+    print(letter_tuples)
 
     return product(*letter_tuples)
 
 def generate_colors_output(text: str) -> str:
     final_string: str = ''
 
+    # use text to generate numbers based on the letters indexes
+    # use those numbers to insert blank points to create shapes
     for colors_product in combine_colors(user_text):
         for color_code in colors_product:
             final_string += '\033[48;5;{}m  '.format(color_code)
